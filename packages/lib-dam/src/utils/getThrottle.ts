9d69591761a691435234501aa37ca07cfbd84a3e
CorentinDoue <corentin.doue.pro@gmail.com>
2025-09-20 21:04:06 +0200

    chore: upgrade throttled-queue

--- packages/lib-dam/src/utils/getThrottle.ts
+++ packages/lib-dam/src/utils/getThrottle.ts
@@ -1,8 +1,12 @@
-import throttledQueue from 'throttled-queue';
+import { throttledQueue } from 'throttled-queue';
 
 export const getThrottle = (
   rateLimit = Infinity,
 ): (<RESPONSE>(throttledFn: () => Promise<RESPONSE>) => Promise<RESPONSE>) =>
   rateLimit === Infinity
     ? throttledFn => throttledFn()
-    : throttledQueue(rateLimit, 1000, true);
+    : throttledQueue({
+        maxPerInterval: rateLimit,
+        interval: 1000,
+        evenlySpaced: true,
+      });
